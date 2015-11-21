7d85ab53157fcf019a7ddfc6d9ffc77016cad9c0
ItsMeAlves <alves.20989@gmail.com>
2015-11-20 23:17:58 -0300

    Função que gera o array inicial foi implementada

--- sudoku.scala
+++ sudoku.scala
@@ -3,7 +3,28 @@ import org.jscala.JArray
 
 object main extends App {
     val js = javascript {
-        val fields = document.getElementById("fields")
+    	def getStartingArray():JArray[JArray[Int]] = {
+	    	val array:JArray[JArray[Int]] = JArray()
+	    	var column:Int = 1
+	    	var row:Int = 1
+
+	    	for(row <- 1 to 9) {
+	    		val aux:JArray[Int] = JArray();
+		    	for(column <- 1 to 9) {
+		    		try {
+		    			val element:String = (document.getElementById
+		    			("l" + row + "c" + column).textContent)
+		    			aux.push(parseInt(element))
+		    		} catch {
+		    			case ex: NumberFormatException => {
+		    				aux.push(-1)
+		    			}
+		    		}
+		    	}
+		    	array.push(aux)
+	    	}
+	    	return array
+    	}
     }
     println(js.asString)
 }
